b40685074e55341c2279156c1cf821ce0aecc1d3
unknown <josemari94@gmail.com>
2024-09-13 15:54:29 +0200

    create NavBar and NarBar-mobile with design implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
+import MobileNav from '@/Components/MobileNav';
 import Nav from '@/Components/Nav';
-import React from 'react'
+import React, { useState } from 'react'
 
 const Homepage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
   return <div className='overflow-x-hidden'>
     <div>
-      <Nav />
+      {/* NavBar */}
+      <MobileNav nav={nav} closeNav={closeNav}/>
+      <Nav openNav={openNav}/>
+      {/* Hero section */}
     </div>
-
   </div>;
   
 };
