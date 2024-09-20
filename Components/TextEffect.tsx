302419197abc500e69a4d0fa2f132f9fe4ee4db2
unknown <josemari94@gmail.com>
2024-09-20 18:34:10 +0200

    add info about user: name, studies,  photo and buttom download CV

---
+++ Components/TextEffect.tsx
@@ -0,0 +1,21 @@
+import { TypeAnimation } from 'react-type-animation';
+
+const TextEffect = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        
+        'Desarrollador de software',
+        1500, 
+        'Abogado',
+        1500,
+      ]}
+      
+      speed={50}
+      className='text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase'
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TextEffect;
