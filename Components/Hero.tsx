0ac29093125507ca8c6ac342ba30b853484c71e2
unknown <josemari94@gmail.com>
2024-09-20 13:10:36 +0200

    add banner and name

---
+++ Components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Particle from './Particle'
+
+const Hero = () => {
+  return (
+  <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
+    <Particle />
+    <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+        <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+            HOLA, SOY <span className='text-green-400'>JOSÉ MARÍA GIL!</span>
+        </h1>
+    </div>
+  </div>
+  );
+};
+
+export default Hero;
